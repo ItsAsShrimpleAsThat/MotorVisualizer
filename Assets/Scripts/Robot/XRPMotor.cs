2b53b71a447da5d7ad996bf876a12a7bd9f2629a
ItsAsShrimpleAsThat <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2024-12-01 11:06:40 -0800

    Motor accel

--- Assets/Scripts/Robot/XRPMotor.cs
+++ Assets/Scripts/Robot/XRPMotor.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class XRPMotor : MonoBehaviour
 {
     public float maxSpeed;
-    public float speed;
+    public float idealSpeed;
+    public float realSpeed;
     public Transform rotorTransform;
     public float rotation;
     public bool isInverted = false;
     public float motorSpeedRandomness;
+    public float motorAcceleration;
 
     public Encoder encoder;
     public void set(double speed)
     {
-        this.speed = (float)speed;   
+        this.idealSpeed = (float)speed;   
     }
 
     public void setInverted(bool inverted)
@@ -35,7 +37,8 @@ public class XRPMotor : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rotation += Mathf.Clamp(speed, -1.0f, 1.0f) * (maxSpeed + Random.Range(-motorSpeedRandomness, motorSpeedRandomness)) * (isInverted ? -1 : 1);
+        realSpeed = Mathf.MoveTowards(realSpeed, idealSpeed, motorAcceleration);
+        rotation += Mathf.Clamp(realSpeed, -1.0f, 1.0f) * (maxSpeed + Random.Range(-motorSpeedRandomness, motorSpeedRandomness)) * (isInverted ? -1 : 1);
         rotorTransform.localRotation = Quaternion.Euler(rotation, 0.0f, 0.0f);
 
         // update encoder- we can't use the rotor's transform rotation because unity wraps object rotations to 0 when >360
